b72bad74c18b5ac49d296809407f18b550767a56
Andrew Santosa <dcsandr@nus.edu.sg>
2016-05-17 09:47:15 +0800

    Corrected path in src/compiler/GUIFrontEnd/zgrviewer/run.sh

--- src/compiler/GUIFrontEnd/zgrviewer/run.sh
+++ src/compiler/GUIFrontEnd/zgrviewer/run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# If you want to be able to run ZGRViewer from any directory,
-# set ZGRV_HOME to the absolute path of ZGRViewer's main directory
-# e.g. ZGRV_HOME=/usr/local/zgrviewer
+# Set the default value for ZGRV_HOME
 
-ZGRV_HOME=/home/jorge/SvnReps/Systems/TRACER/trunk/src/java/GUIFrontEnd/zgrviewer
-#ZGRV_HOME=.
+SCRIPT=$(readlink -f "$0")
+ZGRV_HOME=$(dirname "$SCRIPT")
 
 java -jar "$ZGRV_HOME/target/zgrviewer-0.8.2.jar" "$@"
